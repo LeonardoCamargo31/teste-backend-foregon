0c55ee58e2b04cf5fec31c7b0fecb33bba8cd0f9
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2019-08-20 17:07:33 -0300

    Adicionando try/catch

--- src/controllers/solicitation.js
+++ src/controllers/solicitation.js
@@ -8,36 +8,46 @@ const { extractErrors } = require('../utils/index')
 const partial = async (req, res) => {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
-        //checar se tem token
-        //se já tiver, só atualiza os dados
-        if (req.body.token == undefined) {
-            //salvar uma nova solicitação
-            const { product, name, email, cpf, birthdate, phone } = req.body
-            const solicitation = new Solicitation({ productId: product, name, email, cpf, birthdate, phone })
-            await solicitation.save()
-            res.status(201).json({
-                success: true,
-                title: 'Salvo com sucesso.',
-                status: 201,
-                token: solicitation._id
-            })
-        } else {
-            //tenho que atualizar os dados
-            const solicitation = await Solicitation.findById(req.body.token)
-            solicitation.productId = req.body.product
-            solicitation.name = req.body.name
-            solicitation.email = req.body.email
-            solicitation.cpf = req.body.cpf
-            solicitation.birthdate = req.body.birthdate
-            solicitation.phone = req.body.phone
+        try {
+            //checar se tem token
+            //se já tiver, só atualiza os dados
+            if (req.body.token == undefined) {
+                //salvar uma nova solicitação
+                const { product, name, email, cpf, birthdate, phone } = req.body
+                const solicitation = new Solicitation({ productId: product, name, email, cpf, birthdate, phone })
+                await solicitation.save()
+                res.status(201).json({
+                    success: true,
+                    title: 'Salvo com sucesso.',
+                    status: 201,
+                    token: solicitation._id
+                })
+            } else {
+                //tenho que atualizar os dados
+                const solicitation = await Solicitation.findById(req.body.token)
+                solicitation.productId = req.body.product
+                solicitation.name = req.body.name
+                solicitation.email = req.body.email
+                solicitation.cpf = req.body.cpf
+                solicitation.birthdate = req.body.birthdate
+                solicitation.phone = req.body.phone
 
-            await solicitation.save()
-            res.status(200).json({
-                success: true,
-                title: 'Atualizado com sucesso.',
-                status: 200,
-                token: solicitation._id
-            })
+                await solicitation.save()
+                res.status(200).json({
+                    success: true,
+                    title: 'Atualizado com sucesso.',
+                    status: 200,
+                    token: solicitation._id
+                })
+            }
+        }
+        catch (error) {
+            return res.status(500).json({
+                success: false,
+                title: 'Ocorreu um erro interno.',
+                status: 500,
+                errors: []
+            });
         }
     } else {
         //erro na validação dos dados
@@ -57,37 +67,49 @@ const final = async (req, res) => {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
 
-        //verificar se já tem solicitação com esse cpf
-        const solicitations = await Solicitation
-            .findOne({ cpf: req.body.cpf, complete: true })
-            .sort({ date: -1 })
+        try {
+            //verificar se já tem solicitação com esse cpf
+            const solicitations = await Solicitation
+                .findOne({ cpf: req.body.cpf, complete: true })
+                .sort({ date: -1 })
 
-        const difference = solicitations? moment().diff(solicitations.date, 'days') : 100
-        
-        if (difference >= 90) {
-            const solicitation = await Solicitation.findById(req.body.token)
-            solicitation.productId = req.body.product
-            solicitation.name = req.body.name
-            solicitation.email = req.body.email
-            solicitation.cpf = req.body.cpf
-            solicitation.birthdate = req.body.birthdate
-            solicitation.phone = req.body.phone
-            solicitation.complete = true
-            solicitation.date = new Date()
+            const difference = solicitations ? moment().diff(solicitations.date, 'days') : 100
 
-            await solicitation.save()
-            res.status(201).json({
-                success: true,
-                token: solicitation._id,
-                title: 'Solicitação criada com sucesso.',
-                status: 201
-            })
-        } else {
-            //após uma solicitação, um mesmo CPF não pode ter uma nova proposta por 90 dias.
-            return res.status(400).json({
+            if (difference >= 90) {
+                const solicitation = await Solicitation.findById(req.body.token)
+                solicitation.productId = req.body.product
+                solicitation.name = req.body.name
+                solicitation.email = req.body.email
+                solicitation.cpf = req.body.cpf
+                solicitation.birthdate = req.body.birthdate
+                solicitation.phone = req.body.phone
+                solicitation.complete = true
+                solicitation.date = new Date()
+
+
+                await solicitation.save()
+                res.status(201).json({
+                    success: true,
+                    token: solicitation._id,
+                    title: 'Solicitação criada com sucesso.',
+                    status: 201
+                })
+
+            } else {
+                //após uma solicitação, um mesmo CPF não pode ter uma nova proposta por 90 dias.
+                return res.status(400).json({
+                    success: false,
+                    title: 'Um mesmo CPF não pode ter uma nova proposta por 90 dias.',
+                    status: 400,
+                    errors: []
+                });
+            }
+        }
+        catch (error) {
+            return res.status(500).json({
                 success: false,
-                title: 'Um mesmo CPF não pode ter uma nova proposta por 90 dias.',
-                status: 400,
+                title: 'Ocorreu um erro interno.',
+                status: 500,
                 errors: []
             });
         }
